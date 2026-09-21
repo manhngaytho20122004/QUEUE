fd1ea71480fa13942adb0cdd419889d28826ef11
luongmanh0353@gmail.com <luongmanh0353@gmail.com>
2026-09-21 21:26:32 +0700

    up 1

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -36,5 +36,8 @@ internal class Program
             Console.WriteLine("------------------------");
         }
         Console.WriteLine("XIn chao cac ban");
+        Console.WriteLine("XIn chao cac ban");
+
+
     }
 }
